56b8efc70fd715d3045e183a2d529c2034f17f45
Uítalo <uitalo.urss@gmail.com>
2022-05-18 23:17:27 -0300

    inserção de ilustrações

--- src/components/AssinaturaNewsLetter/index.jsx
+++ src/components/AssinaturaNewsLetter/index.jsx
@@ -1,18 +1,24 @@
 import './style.css'
+import plant from '../../assets/imagem-hero-1.png';
 
 export function AssinaturaNewsLetter(){
   return(
     <div className='intro'>
-      <div className='text-newsletter'>
-        <p className='title'>Sua casa com as </p>
-        <p className='special'>melhores plantas</p>
-        <p className='description'>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
-      </div>
       <div className='newsletter'>
-        <input 
-        type="text"
-        placeholder='insira o seu e-mail' />
-        <button type='button'>Assinar Newsletter</button>
+        <div className='text-newsletter'>
+          <p className='title'>Sua casa com as </p>
+          <p className='special'>melhores plantas</p>
+          <p className='description'>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+        </div>
+        <div className='form-newsletter'>
+          <input 
+          type="text"
+          placeholder='insira o seu e-mail' />
+          <button type='button'>Assinar Newsletter</button>
+        </div>
+      </div>
+      <div className='ilustration'>
+        <img src={plant} alt="ilustração planta" />
       </div>
     </div>
   )
