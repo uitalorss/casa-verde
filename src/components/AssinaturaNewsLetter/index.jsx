042c8a10df655ddbbb2aab177d943f81aa6f3f5b
Uítalo <uitalo.urss@gmail.com>
2022-05-18 22:12:24 -0300

    parte textual do componente newsletter

--- src/components/AssinaturaNewsLetter/index.jsx
+++ src/components/AssinaturaNewsLetter/index.jsx
@@ -2,11 +2,18 @@ import './style.css'
 
 export function AssinaturaNewsLetter(){
   return(
-    <div className='newsletter'>
-      <input 
-      type="text"
-      placeholder='insira o seu e-mail' />
-      <button type='button'>Assinar Newsletter</button>
+    <div className='intro'>
+      <div className='text-newsletter'>
+        <p className='title'>Sua casa com as </p>
+        <p className='special'>melhores plantas</p>
+        <p className='description'>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+      </div>
+      <div className='newsletter'>
+        <input 
+        type="text"
+        placeholder='insira o seu e-mail' />
+        <button type='button'>Assinar Newsletter</button>
+      </div>
     </div>
   )
 }
