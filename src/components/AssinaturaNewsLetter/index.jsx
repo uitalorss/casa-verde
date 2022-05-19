0ebbf1221794e328725859e4171fd1a373a92ea0
Uítalo <uitalo.urss@gmail.com>
2022-05-18 21:48:08 -0300

    componente de assinatura de newsletter

--- src/components/AssinaturaNewsLetter/index.jsx
+++ src/components/AssinaturaNewsLetter/index.jsx
@@ -2,6 +2,11 @@ import './style.css'
 
 export function AssinaturaNewsLetter(){
   return(
-    <></>
+    <div className='newsletter'>
+      <input 
+      type="text"
+      placeholder='insira o seu e-mail' />
+      <button type='button'>Assinar Newsletter</button>
+    </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import { AssinaturaNewsLetter } from '../../components/AssinaturaNewsLetter'
 import { Menu } from '../../components/Menu'
 import './style.css'
 function App() {
   return (
     <div className="container">
       <Menu />
+      <AssinaturaNewsLetter />
     </div>
   )
 }
